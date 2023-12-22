82fe88550ef49bd61d51a896f660c9ade689bf5d
Hardi Rachh <shahhardi71@gmail.com>
2023-12-22 19:50:36 +0000

    Added 2 activities sender and receiver according to our concept of demo.

--- app/src/main/java/com/hardi/starterpatterndemo/ReceiverActivity.kt
+++ app/src/main/java/com/hardi/starterpatterndemo/ReceiverActivity.kt
@@ -3,9 +3,9 @@ package com.hardi.starterpatterndemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ReceiverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_receiver)
     }
 }
---
+++ app/src/main/java/com/hardi/starterpatterndemo/SenderActivity.kt
@@ -0,0 +1,11 @@
+package com.hardi.starterpatterndemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SenderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sender)
+    }
+}
