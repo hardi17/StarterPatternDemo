a40918f2ed4ccccd554bb5c6f83eef51d01b0df5
Hardi Rachh <shahhardi71@gmail.com>
2023-12-22 23:04:33 +0000

    Implement starter pattern.

--- app/src/main/java/com/hardi/starterpatterndemo/ReceiverActivity.kt
+++ app/src/main/java/com/hardi/starterpatterndemo/ReceiverActivity.kt
@@ -1,11 +1,38 @@
 package com.hardi.starterpatterndemo
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hardi.starterpatterndemo.databinding.ActivityReceiverBinding
 
 class ReceiverActivity : AppCompatActivity() {
+
+    private lateinit var bindng: ActivityReceiverBinding
+
+    companion object{
+
+        const val EXTRAS_MONEY = "EXTRAS_MONEY"
+
+        fun getStartIntent(context: Context, money: String) : Intent {
+            return Intent(context, ReceiverActivity::class.java)
+                .apply {
+                    putExtra(EXTRAS_MONEY,money)
+                }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_receiver)
+        bindng= ActivityReceiverBinding.inflate(layoutInflater)
+        setContentView(bindng.root)
+        getIntentData()
+    }
+
+    private fun getIntentData() {
+        val money= intent.getStringExtra(EXTRAS_MONEY)
+        money.let {
+            bindng.tvReceiver.text = "Hey hardy, \n Thank you for sending \n $$money."
+        }
     }
 }
--- app/src/main/java/com/hardi/starterpatterndemo/SenderActivity.kt
+++ app/src/main/java/com/hardi/starterpatterndemo/SenderActivity.kt
@@ -2,10 +2,33 @@ package com.hardi.starterpatterndemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.hardi.starterpatterndemo.databinding.ActivitySenderBinding
 
 class SenderActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySenderBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sender)
+        binding= ActivitySenderBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        sendMoney()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.edAmount.text = null
+    }
+
+    private fun sendMoney() {
+        binding.btnSend.setOnClickListener{
+            val money = binding.edAmount.text.toString().trim()
+            if(money.isNotEmpty()){
+                startActivity(ReceiverActivity.getStartIntent(this@SenderActivity,money))
+            }else{
+                Toast.makeText(this,"Please add some amount, Thank You!!!",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
